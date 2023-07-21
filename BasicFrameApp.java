51d7b1e03485112f1b3529acaa39a6b82699bb68
maxwellmnguni <140168792+maxwellmnguni@users.noreply.github.com>
2023-07-21 13:47:15 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,10 @@
+
+package basicframeapp;
+import za.ac.tut.MyFirstFrame;
+public class BasicFrameApp {
+
+    public static void main(String[] args) {
+        MyFirstFrame myFrame = new MyFirstFrame();
+    }
+    
+}
---
+++ MyFirstFrame.java
@@ -0,0 +1,18 @@
+
+package za.ac.tut;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class MyFirstFrame extends JFrame{
+    public MyFirstFrame(){
+        setTitle("My First GUI");
+        setSize(200, 250);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.BLUE);
+        setVisible(true);
+        
+    }
+}
